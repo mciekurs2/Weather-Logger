2d24313e40844b17de8178adb31c6c5a9392e7f0
martin <mciekurs@outlook.com>
2018-08-22 14:47:50 +0300

    Finalizing permission check, fixing bugs

--- app/src/main/java/lv/mciekurs/weatherlogger/MainActivity.kt
+++ app/src/main/java/lv/mciekurs/weatherlogger/MainActivity.kt
@@ -2,24 +2,36 @@ package lv.mciekurs.weatherlogger
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.pm.PackageManager
+import android.app.Activity
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
+import android.support.design.widget.Snackbar
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import com.nabinbhandari.android.permissions.PermissionHandler
+import com.nabinbhandari.android.permissions.Permissions;
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
     private var ACCESS_COARSE_LOCATION_CODE = 123
+    private lateinit var dialog: Dialog
     private lateinit var list: MutableList<WeatherInfo>
     private lateinit var adapter: RecyclerViewAdapter
 
@@ -35,6 +47,14 @@ class MainActivity : AppCompatActivity() {
         recyclerView_main.layoutManager = layoutManager
         recyclerView_main.adapter = adapter
 
+    }
+
+    private fun getLocation(){
+        locationRequest = LocationRequest.create()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 100
+        locationRequest.fastestInterval = 100
+
 
 
     }
@@ -45,8 +65,20 @@ class MainActivity : AppCompatActivity() {
         var lon = 0.0
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        fusedLocationClient.lastLocation.addOnCompleteListener {
-            if (it.isSuccessful){
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            if (location != null) {
+                lat = location.latitude
+                lon = location.longitude
+
+                Toast.makeText(applicationContext, "$lat, $lon", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "Unsuccessful conn", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+/*            if (it.isSuccessful){
                 lat = it.result.latitude
                 lon = it.result.longitude
 
@@ -54,41 +86,30 @@ class MainActivity : AppCompatActivity() {
             }
             else {
                 Toast.makeText(applicationContext, "Unsuccessful conn", Toast.LENGTH_SHORT).show()
-            }
-        }
+            }*/
+
 
         val url = "http://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&units=metric&appid=65b9e5e5bf3289000b4d663073142565"
     }
 
     override fun onStart() {
         super.onStart()
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            //permission not granted
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                            Manifest.permission.ACCESS_COARSE_LOCATION)) {
-                //show an explanation to the user
-            } else {
-                //request the permission
-                ActivityCompat.requestPermissions(this,
-                        arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
-                        ACCESS_COARSE_LOCATION_CODE)
-            }
-        }
+        Permissions.check(this, Manifest.permission.ACCESS_COARSE_LOCATION, null, object : PermissionHandler() {
+            override fun onGranted() {}
+        })
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        when(requestCode){
-            ACCESS_COARSE_LOCATION_CODE -> {
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    //permission granted
-                } else {
-                    //permission denied
-                }
-                return
+    private fun checkPermission(){
+        Permissions.check(this, Manifest.permission.ACCESS_COARSE_LOCATION, null, object : PermissionHandler() {
+            override fun onGranted() {
+                snackbar("Location permission has been granted.")
             }
-        }
+
+            override fun onDenied(context: Context?, deniedPermissions: ArrayList<String>?) {
+                snackbar("Location permission has been denied.")
+            }
+        })
+
     }
 
     private fun addItem(){
@@ -109,12 +130,20 @@ class MainActivity : AppCompatActivity() {
             R.id.item_save -> {
                 //TODO: Add necessary function
                 addItem()
-                //Toast.makeText(applicationContext, "Test Toast", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.item_alert -> {
+                //TODO: Add settings function
+                checkPermission()
                 return true
             }
         }
         return super.onOptionsItemSelected(item)
     }
+
+    //simple snackbar function
+    private fun Activity.snackbar(message: CharSequence) =
+            Snackbar.make(root_layout, message, Toast.LENGTH_SHORT).show()
 
 
 }
